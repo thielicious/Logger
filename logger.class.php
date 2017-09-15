3aa87843830e4be55335e74df6754b5defe24c6e
Thielicious <TheDefinitionist@users.noreply.github.com>
2017-09-16 00:58:46 +0200

    Update logger.class.php

--- logger.class.php
+++ logger.class.php
@@ -14,15 +14,15 @@
 			$file,
 			$prefix;
 			
-		public function __construct($filename) {
+		public function __construct(string $filename) {
 			$this->file = $filename;
 		}
 		
-		public function setTimestamp($format) {
+		public function setTimestamp(string $format) {
 			$this->prefix = "<strong>".date($format)." &raquo; </strong>";
 		}
 		
-		public function putLog($insert) {
+		public function putLog(string $insert) {
 			if (isset($this->prefix)) {
 				file_put_contents($this->file, $this->prefix.$insert."<br>", FILE_APPEND);
 			} else {
