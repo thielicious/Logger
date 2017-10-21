9ba972b24129b276e649bbfc3b88dd159b65c76b
Thielicious <TheDefinitionist@users.noreply.github.com>
2017-10-21 22:58:02 +0200

    Update logger.class.php

--- logger.class.php
+++ logger.class.php
@@ -4,7 +4,7 @@
 		Logger v1.0
 		(c) 2017 by Thielicious
 		
-		Logger is a simple class in PHP that lets you record actions done by clients in a txt file.
+		Logger is a simple class that lets you record done actions in a text file.
 	*/
 
 
@@ -26,13 +26,15 @@
 			if (isset($this->prefix)) {
 				file_put_contents($this->file, $this->prefix.$insert."<br>", FILE_APPEND);
 			} else {
-				echo "<script>alert(\"Timestamp not set yet.\");</script>", die;
+				trigger_error("Timestamp not set", E_USER_ERROR); & die;
 			}
 		}
 		
 		public function getLog() {
-			$content = @file_get_contents($this->file);
-			return $content;
+			if (isset($this->file)) {
+				$content = @file_get_contents($this->file);
+				return $content;
+			}
 		}
 		
 		public function clear() {
